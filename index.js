94cd9243e465c852600a62995061f889f3c0c170
Elvis Vizcaino <elvizcaino@gmail.com>
2020-09-02 18:30:49 -0500

    Fin sección 3

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+// App de Express
+const app = express();
+
+// Node server
+const server = require("http").createServer(app);
+module.exports.io = require('socket.io')(server);
+require("./sockets/socket");
+
+// Path público
+const publicPah = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPah));
+
+server.listen(process.env.PORT, (err) => {
+    if(err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto`, process.env.PORT);
+});
